f4315b22c1831f7749e6859841dfd6dd0ac780b7
robert_gevorgyan <gevorgyanrk@gmail.com>
2021-04-16 15:58:27 +0300

    Домашнее задание к уроку 3 - MVC

---
+++ spring-MVC/src/main/java/com/example/controllers/TaskController.java
@@ -0,0 +1,52 @@
+package com.example.controllers;
+
+import com.example.model.Product;
+import com.example.model.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.math.BigDecimal;
+
+@Controller
+@RequestMapping("/product")
+public class TaskController {
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	@GetMapping
+	public String indexPage(Model model) {
+		model.addAttribute("products", productRepository.findAll());
+		return "task_views/index";
+	}
+
+	@GetMapping("/{id}")
+	public String editProduct(@PathVariable(value = "id") Long id,
+							  Model model) {
+		model.addAttribute("product", productRepository.findById(id));
+		return "task_views/product_form";
+	}
+
+	@PostMapping("/product_update")
+	public String updateProduct(Product product) {
+		productRepository.update(product);
+		return "redirect:/product";
+	}
+
+	@GetMapping("/new")
+	public String newProduct(Model model) {
+		model.addAttribute("product", new Product((productRepository.getMaxId()+1L), null, null, null));
+		return "task_views/product_form";
+	}
+
+	@GetMapping("/delete/{id}")
+	public String removeProduct(@PathVariable(value = "id") Long id) {
+		productRepository.remove(id);
+		return "redirect:/product";
+	}
+}
